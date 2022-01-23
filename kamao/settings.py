43ff55e326ce602edc7f3e666e670e80111eb6d4
Nischal2015 <nischalshakya2015@gmail.com>
2022-01-23 22:28:57 +0545

    added frontend

--- kamao/settings.py
+++ kamao/settings.py
@@ -54,7 +54,9 @@ ROOT_URLCONF = 'kamao.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'frontend/build'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'frontend/build/static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
